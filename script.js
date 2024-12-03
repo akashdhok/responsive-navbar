f7502c73123edde4d8c59d94a33733a9204464fa
Akash Dhok <akashdhok1265@gmail.com>
2024-12-03 19:29:27 +0530

    commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function run()
+{
+    let a = document.querySelector("#side")
+    a.style.display = "grid"
+    a.style.transition = "1s"
+    let b = document.querySelector("#cross")
+    b.style.display = "block"
+     
+}
+function cross()
+{
+     let a = document.querySelector("#side")
+    a.style.display = "none"
+      let b = document.querySelector("#cross")
+    b.style.display = "none"
+}
